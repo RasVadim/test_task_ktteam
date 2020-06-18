2dfaa576c8f2949a3c9e91a798f2abe36122b3a4
RasVadim <rasstriginv@gmail.ru>
2020-06-18 21:03:55 +0400

    triandle + square

--- src/components/CanvasTrianle.vue
+++ src/components/CanvasTrianle.vue
@@ -12,57 +12,54 @@ export default {
   }
 };
 
-
-window.onload = function() {  
+window.onload = function() {
   const canv = document.getElementById("canvas"),
     ctx = canv.getContext("2d");
 
-  canv.width = "500";
-  canv.height = "500";
+  canv.width = "501";
+  canv.height = "501";
 
-  ctx.fillStyle = "green";
-  ctx.strokeStyle = "green";
-  ctx.lineWidth = 2;
+  function setPixel(point, red=255, blue=255, green=255, alpha=255) {
+    // point: [x, y]
+    var img = ctx.createImageData(1, 1);
+    img.data[0] = red; // red
+    img.data[1] = green; // green
+    img.data[2] = blue; // blue
+    img.data[3] = alpha; // alpha
+    ctx.putImageData(img, point[0], point[1]);
+  }
 
+  let A = [0, 0],
+      B = [500, 0],
+      C = [500, 500],
+      D = [0, 500],
+      N = [250, 250];
 
-  let 
-  xA = 250, 
-  yA = 0, 
-  xB = 0, 
-  yB = 470, 
-  xC = 500, 
-  yC = 470;
-  
-setPixel([250,250]);
+  let tops = [A, B, C, D];
 
-  ctx.beginPath();
-  ctx.moveTo(xA, yA);
-  ctx.lineTo(xB, yB);
-  ctx.lineTo(xC, yC);
-  ctx.closePath();
-  ctx.stroke();
+  for (let top in tops) {
+     setPixel(tops[top]);
+  };
 
+  setPixel(N);
 
+  let xN = N[0],
+      yN = N[1];
 
+  setInterval(function() {
+    for (let i = 0; i < 30; i++) {
+      let M = tops[Math.floor(Math.random() * 3)];
+      xN = (xN + M[0]) / 2;
+      yN = (yN + M[1]) / 2;
 
-  // ctx.arc(250, 250, 50, 0, Math.PI *2);
-  // ctx.stroke();
-  // setInterval(function() {
-  //   ctx.fillStyle = "white";
-  //   ctx.fillRect(0, 0, canv.width, canv.height);
-  //   ctx.fillStyle = "green";
-  //   ctx.fillRect(x++, 10, 150, 150);
-  // }, 10);
-
-function setPixel(point) { // point: [x, y]
-    var img = canv.createImageData(1,1);
-    img.data[0]=0 // red
-    img.data[1]=0 // green
-    img.data[2]=0 // blue
-    img.data[3]=255 // alpha
-    canv.putImageData(img,point[0],point[1]);
-};
-
+      let red = Math.floor(xN *255/500), 
+          blue =  Math.floor(255 - xN *255/500),
+          green =  Math.floor(255 - yN *255/500),
+          alpha = 255;
+      setPixel([xN, yN], red, blue, green, alpha);
+      // return xN, yN;
+    }
+  }, 100);
 };
 </script>
 
@@ -73,6 +70,6 @@ function setPixel(point) { // point: [x, y]
 }
 
 #canvas {
-  border: 1px solid green;
+  // border: 1px solid green;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,6 +19,7 @@ export default {
 
 <style lang="scss" scoped>
 .home {
+  background-color: black;
   img {
     height: 50px;
   }
